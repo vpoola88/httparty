1a0085bb88f33156fe37e71634a35bb7ba1baa4a
Vivek Poola <vpoola88@gmail.com>
2016-08-22 19:08:23 -0700

    rename repo, add json from github

--- app.rb
+++ app.rb
@@ -1,17 +1,39 @@
 require 'sinatra'
 require 'json'
+require 'pry'
+require 'httparty'
 
+  
 post '/payload' do
   push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
+  binding.pry
+  user = push['sender']['login']
 
-  hello_world
-  
+  # data = {"currentUserId": currentUserId, }
+
+  if status 200
+    u = UserCommits.new(user)
+    puts u.get_talentlms_api_data
+  else
+
+  end
 end 
 
+class UserCommits
+  def initialize(username)
+    @username = username
+  end
 
-def hello_world
-  p "$" * 100
-  puts "helo world"
-  p "$" * 100
+  def github_url
+    "https://api.github.com/users/#{@username}"
+  end
+
+  def get_talentlms_api_data
+    response = HTTParty.get(github_url)
+    data = JSON.parse(response.body)
+
+    binding.pry
+  end
+
 end
+
